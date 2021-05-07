2d032a05867fc2ea6b29d5a03ec554e527a74c00
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2021-05-07 14:40:56 +0200

    Added Attribution Section, added attribution text style

--- src/components/attribution/Attribution.js
+++ src/components/attribution/Attribution.js
@@ -3,7 +3,7 @@ import './Attribution.css';
 const Attribution = () => {
     return ( 
         <div className='attribution'>
-            <p>Attribution Section</p>
+            <p>Created by <a href='https://github.com/IevgeniiaAbdulina/Weather-App-Challenge' target='_blank'>Ievgeniia Abdulina</a> - devChallenges.io</p>
         </div>
      );
 }
